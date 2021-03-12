18524ab714c3d2408640343873e6d2e2763ee769
Vignesh waran <vw170996@gmail.com>
2021-03-12 22:31:59 +0530

    Style ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,10 +3,16 @@ import './ColorBox.css'
 
 export default class ColorBox extends Component {
   render() {
+    const {name, bg} = this.props
     return (
-      <div style={{background: this.props.bg}} className="ColorBox">
-      <span>{this.props.name}</span>
-        <span>MORE</span>
+      <div style={{background: bg}} className="ColorBox">
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-btn">Copy</button>
+        </div>
+        <span className="see-more">more</span>
       </div>
     )
   }
